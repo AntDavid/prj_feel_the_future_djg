b48081849d4e4840fb0a787626265d4ea9286256
ADavid <davidantoniodavi@gmail.com>
2024-01-20 16:06:57 -0300

    Adicionado urls das novas páginas htmls

--- flashcard/urls.py
+++ flashcard/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('novo_flashcard/', views.novo_flashcard, name="novo_flashcard"),
     path('deletar_flashcard/<int:id>', views.deletar_flashcard, name="deletar_flashcard"),
     path('iniciar_desafio/', views.iniciar_desafio, name="iniciar_desafio"),
+    path('listar_desafio/', views.listar_desafio, name="listar_desafio"),
+    path('desafio/<int:id>/', views.desafio, name='desafio'),
+    path('responder/flashcard/<int:id>/', views.responder_flashcard, name='responder_flashcard'),
 ]
